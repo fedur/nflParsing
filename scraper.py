27bc12148a66ccad0c56f87c9dc96efdadb11737
fedur <felix.durand.p@gmail.com>
2020-04-02 21:38:35 -0400

    Working baby

--- scraper.py
+++ scraper.py
@@ -1,6 +1,15 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
+import time
+
+def count(x):
+	totalCount=x
+	while(totalCount>0):
+		time.sleep(1)
+		print(totalCount)
+		totalCount -= 1
+
 
 def scrape(url):
 	def findDiv(driver):
@@ -8,6 +17,9 @@ def scrape(url):
 
 	driver = webdriver.Chrome()
 	driver.get(url)
-	element = WebDriverWait(driver, 10).until(findDiv)
-	return element.text;
-	
+	menu = WebDriverWait(driver, 5).until(findDiv)
+	print("sleepeuh")
+	count(10)
+	ActionChains(driver).move_to_element(menu).move_by_offset(0, 100).click().perform()
+	csvData = driver.find_element_by_id("csv_receiving_advanced")
+	return csvData.text
