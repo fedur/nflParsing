179f2f8b03b332b57eb979ceadb38b7da8545770
fedur <felix.durand.p@gmail.com>
2020-05-02 14:54:38 -0400

    Old method was somehow not working due to problem with scrolling, is now working again

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import time
 
@@ -14,12 +15,12 @@ def count(x):
 def scrape(url):
 	def findDiv(driver):
 		return driver.find_element_by_xpath("//div[@id='all_receiving_advanced']//ul/li")
-
-	driver = webdriver.Chrome()
+	options = webdriver.ChromeOptions()
+	driver = webdriver.Chrome(chrome_options=options)
 	driver.get(url)
 	menu = WebDriverWait(driver, 5).until(findDiv)
-	print("sleepeuh")
-	count(10)
-	ActionChains(driver).move_to_element(menu).move_by_offset(0, 100).click().perform()
+	ActionChains(driver).move_to_element(menu).send_keys(Keys.PAGE_DOWN).perform()
+	count(1)
+	ActionChains(driver).move_to_element(menu).move_by_offset(0,100).click().perform()
 	csvData = driver.find_element_by_id("csv_receiving_advanced")
 	return csvData.text
